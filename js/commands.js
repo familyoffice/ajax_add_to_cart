3d07dd11d06e578e72633c81ebdb1c61d2c8ec20
Harsh Behl <harsh.behl@tothenew.com>
2017-10-06 13:13:13 +0530

    chaning js for closing dialog after product addition after 2 seconds

--- js/commands.js
+++ js/commands.js
@@ -1,6 +1,9 @@
 (function ($, Drupal) {
   'use strict';
   Drupal.AjaxCommands.prototype.reload = function (ajax, response, status) {
-    $('#drupal-modal a.cart-block--link__expand').click();
+    setTimeout(
+    function() {
+      $(".ui-dialog .ui-button").click();
+    }, 2000);
   };
 })(jQuery, Drupal);
